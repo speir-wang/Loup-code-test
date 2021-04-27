ed0ef61a517dbe9060f0b570f458d998bc288003
He Wang <speir.wang@gmail.com>
2021-04-27 10:17:22 +1000

    feat: set up the app main UI with mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
 import * as React from 'react'
+import styled from 'styled-components'
+import { Recipe } from './components/Recipe'
+import { RecipeList } from './components/Recipe/Recipes.styles'
+import type { Recipe as RecipeType } from './types/app'
 
-function App() {
-  return <div className="App"></div>
+const AppWrapper = styled.div`
+  max-width: 1024px;
+  margin: 0 auto;
+  padding: 16px;
+  background-color: #f4f4f4;
+`
+
+interface Props {
+  data: {
+    contents: RecipeType[]
+  }
+}
+
+function App({ data }: Props) {
+  const [recipes] = React.useState<RecipeType[]>(data.contents)
+
+  return (
+    <AppWrapper>
+      <RecipeList>
+        {recipes.map(recipe => (
+          <Recipe key={recipe.contentId} recipe={recipe} />
+        ))}
+      </RecipeList>
+    </AppWrapper>
+  )
 }
 
 export default App
