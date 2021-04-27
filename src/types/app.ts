45fa2304d3101946dc47bd2b80ccf1d557e46773
He Wang <speir.wang@gmail.com>
2021-04-27 10:02:35 +1000

    fix: update imageList type

--- src/types/app.ts
+++ src/types/app.ts
@@ -3,16 +3,30 @@ export interface Recipe {
   title: string
   urlPartial: string
   isFavourited: boolean
-  imageList: Image
+  imageList: ImageList
   tags: Tag[]
   contentType?: number
 }
 
-export interface Image {
-  [key: string]: {
+export type ImageList = {
+  [key in ImageSrcSet]: {
     url: string
   }
 }
+
+export type ImageSrcSet =
+  | 'landscape32small1x'
+  | 'landscape32small2x'
+  | 'landscape32small3x'
+  | 'landscape32medium1x'
+  | 'landscape32medium2x'
+  | 'landscape32medium3x'
+  | 'landscapemobile1x'
+  | 'landscapemobile2x'
+  | 'landscapemobile3x'
+  | 'landscapewidedesktop1x'
+  | 'landscapewidedesktop2x'
+  | 'landscapewidedesktop3x'
 
 export interface Tag {
   id: number
